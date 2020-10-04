aec4edeb0fa70d02db4fd9bcfc069366280948a0
Aleksandr <mail.bolshakov@gmail.com>
2020-10-04 18:35:27 +0300

    Initial commit

---
+++ shop/src/test/java/pro/bolshakov/geekbrains/springleveltwo/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.bolshakov.geekbrains.springleveltwo.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
