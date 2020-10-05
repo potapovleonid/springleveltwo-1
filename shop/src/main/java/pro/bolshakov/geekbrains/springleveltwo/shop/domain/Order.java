6f6b65231ffdf1f8e1cbd12955a27db154faf317
Aleksandr <mail.bolshakov@gmail.com>
2020-10-05 11:35:10 +0300

    add JPA description for entities

--- shop/src/main/java/pro/bolshakov/geekbrains/springleveltwo/shop/domain/Order.java
+++ shop/src/main/java/pro/bolshakov/geekbrains/springleveltwo/shop/domain/Order.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,14 +16,26 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "orders")
 public class Order {
+    private static final String SEQ_NAME = "order_seq";
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQ_NAME)
+    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME)
     private Long id;
+    @CreationTimestamp
     private LocalDateTime created;
+    @UpdateTimestamp
     private LocalDateTime changed;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
     private BigDecimal sum;
     private String address;
+    @OneToMany(cascade = CascadeType.ALL)
     private List<OrderDetails> details;
-
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 }
--- shop/src/main/java/pro/bolshakov/geekbrains/springleveltwo/shop/domain/Product.java
+++ shop/src/main/java/pro/bolshakov/geekbrains/springleveltwo/shop/domain/Product.java
@@ -5,17 +5,28 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "products")
 public class Product {
+    private static final String SEQ_NAME = "product_seq";
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQ_NAME)
+    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME)
     private Long id;
     private String title;
     private Double price;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "products_categories",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private List<Category> categories;
 
 }
